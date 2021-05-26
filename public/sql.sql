44b8fb07e017a3e8de58837ea20537bd2e803ba8
Josej <josedebonaj@gmail.com>
2021-05-26 17:46:58 -0300

    atualização

---
+++ public/sql.sql
@@ -0,0 +1,19 @@
+USE db_app_db2;
+
+CREATE TABLE autores (nome VARCHAR(100) PRIMARY KEY
+, dt_nasc DATE
+);
+
+CREATE TABLE livros (id_livro INT PRIMARY KEY
+, autor varchar(100) 
+, categoria VARCHAR(100)
+, classificacao VARCHAR(100)
+, editora varchar (50)
+, num_pag INT
+, data_lancamento DATE
+, edicao INT
+, CONSTRAINT autor_fk FOREIGN KEY(autor) REFERENCES autores(nome)
+);
+
+drop table livros;
+drop table autores;
